6900f66ed031240e63436bde6b18916e663c5319
suji.park <charmmming7@gmail.com>
2024-11-05 15:49:07 +0900

    🔨 api 페치 함수 공통화

--- frontend/src/api/useApi.ts
+++ frontend/src/api/useApi.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { LoginParams } from './../store/useAuthStore';
 
 const API_URL = 'http://localhost:8080';
+
 // Axios 인스턴스 생성
 const api = axios.create({
   baseURL: API_URL,
@@ -20,9 +21,29 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+const fetchAPI = async (url: string, method: string, data: any) => {
+  console.log(data);
+  try {
+    const response = await api.request({
+      url: url,
+      method: method,
+      data: data,
+    });
+    console.log('응답:', response.data);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error('API 요청 에러:', error.response?.data);
+    } else {
+      console.error('알 수 없는 에러:', error);
+    }
+    throw error;
+  }
+};
+
 // 1-3) Todos 관련 API 함수
 export const getTodos = async () => {
-  const response = await api.get('/todos');
+  const response = await fetchAPI('/todos', 'GET', null);
   return response.data;
 };
 
@@ -47,22 +68,12 @@ export const deleteTodo = async (id: string) => {
 };
 
 // 1-4) Auth 관련 API 함수
+// 로그인
 export const login = async ({ email, password }: LoginParams) => {
-  console.log(email, password);
-  const response = await api.post('/users/login', { email, password });
-  return response.data;
+  return await fetchAPI('/users/login', 'POST', { email, password });
 };
 
+// 회원가입
 export const signUp = async ({ email, password }: LoginParams) => {
-  // const response = await api.post('/users/create', { email, password });
-  // return response.data;
-  console.log(email, password);
-  try {
-    const response = await axios.post('http://localhost:8080/users/create', { email, password });
-    console.log('응답:', response.data);
-    return response.data;
-  } catch (error) {
-    console.error('API 요청 에러:', error);
-    throw error; // 에러를 다시 던져서 호출한 곳에서 처리하도록 합니다.
-  }
+  return await fetchAPI('/users/create', 'POST', { email, password });
 };
