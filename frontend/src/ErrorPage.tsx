0de28413afd65241c87d67a0b948eb7b104b76ab
suji.park <charmmming7@gmail.com>
2024-11-05 15:50:55 +0900

    💄 에러 페이지 수정

--- frontend/src/ErrorPage.tsx
+++ frontend/src/ErrorPage.tsx
@@ -1,19 +1,40 @@
 import { Link } from 'react-router-dom';
-import { useRouteError } from 'react-router-dom';
 
-interface CustomError extends Error {
-  message: string;
-}
+// interface CustomError extends Error {
+//   message: string;
+// }
 
 const ErrorPage = () => {
-  const error = useRouteError() as CustomError;
+  // const navigate = useNavigate();
+  // const error = useRouteError() as CustomError;
+
+  // useEffect(() => {
+  //   if (error) {
+  //     // 에러가 발생했을 때 메인 페이지로 리다이렉트
+  //     setTimeout(() => {
+  //       navigate('/');
+  //     }, 1000);
+  //   }
+  // }, [error, navigate]);
 
   return (
-    <div>
-      <h1>404 에러</h1>
-      <p>{error.message}</p>
-      <Link to="/">메인 페이지로 가기</Link>
-    </div>
+    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-indigo-600">404</p>
+        <h1 className="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+          Page not found
+        </h1>
+        <p className="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">페이지를 찾을 수 없습니다.</p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            to="/"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            메인으로 돌아가기
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 };
 
