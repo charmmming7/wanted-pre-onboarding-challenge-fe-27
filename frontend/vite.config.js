80d9d0147b450f8bfceec38bbdad890c6f3167a0
suji.park <charmmming7@gmail.com>
2024-11-07 22:19:07 +0900

    ci: vercel 설정파일 프로젝트 루트로 이동, api vite 설정 추가

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,13 +7,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 8090,
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:8080', // 백엔드 API 서버 주소
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, ''), // 요청 경로 수정 (예: /api/user/login -> /user/login)
-    //   },
-    // },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 백엔드 API 서버 주소
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''), // 요청 경로 수정 (예: /api/user/login -> /user/login)
+      },
+    },
   },
   optimizeDeps: {
     include: ['prettier'],
